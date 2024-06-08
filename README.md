015ae36223aa7f2e02f3cdfd9b67d73d44337ee1
Nuno Fernandes <nmfernandes@hotmail.com>
2024-06-08 11:31:50 -0300

    esturtura

---
+++ README.md
@@ -0,0 +1,7 @@
+---
+library_name: peft
+base_model: gpt2
+---
+### Framework versions
+
+- PEFT 0.11.1
---
+++ project/params.py
@@ -0,0 +1,18 @@
+
+
+### Configuraçoes PEFT
+R = 8
+LORA_ALPHA = 32
+LORA_DROPOUT = 0.1
+
+
+### Configurações do Modelo
+MODEL_PATH = 'project/model'
+TRAIN_FILE_PATH = 'raw_data/data_cleaned.csv'
+MODEL_NAME = 'gpt2'
+PER_DEVICE_TRAIN_BATCH_SIZE = 16
+NUM_TRAIN_EPOCHS = 5.0
+SAVE_STEPS = 20
+
+LEARNING_RATE = 5e-3 
+LOGGING_STEPS = 20
