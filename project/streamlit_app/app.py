96075ef45d5c72070f1b02245b2f30f3befe50cc
Nuno Fernandes <nmfernandes@hotmail.com>
2024-06-13 22:48:21 -0300

    mudança pagina resolvida

--- project/streamlit_app/app.py
+++ project/streamlit_app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from navigation import get_navigation
 from confirm import get_confirmation
-from home import home as home_page
+from home import home 
 from gerador import gerador
 from contato import contato
 
@@ -9,27 +9,19 @@ from contato import contato
 
 if "escolha" not in st.session_state:
     st.session_state["escolha"] = 'pergunta'
-else:
-    st.session_state["escolha"] = st.session_state["escolha"] 
 
 
+
+    
 if st.session_state["escolha"] == 'pergunta':
     
-
-    if "nome" not in st.session_state:
-        st.session_state["nome"] = ""
-        
-    st.subheader("Nome do Personagem")
-    nome = st.text_input(f"Nome do Personagem: ", value=st.session_state["nome"])
-
-    if nome != None:
-        st.subheader(nome)
-        
-if st.session_state["escolha"] == 'gerador':
-    gerador()
-
-confirm_button = st.button('Confirmar escolhas')
-if confirm_button:
-    st.session_state["escolha"] = 'gerador'
+    home()
+   
+elif st.session_state["escolha"] == 'gerador':
     
+    contato()
+        
 
+
+
+    
--- project/streamlit_app/contato.py
+++ project/streamlit_app/contato.py
@@ -3,74 +3,11 @@
 import streamlit as st
 
 def contato() -> None:
-
-    # img = get_img_as_base64("images/clouds.png")
-
-    # page_bg_img = f"""
-    # <style>
-    # [data-testid="stAppViewContainer"] > .main {{
-    # background-image: url("data:image/png;base64,{img}");
-    # background-size: 100%;
-    # background-position: center;
-    # background-repeat: no-repeat;
-    # background-attachment: fixed;
-    # }}
-
-    # [data-testid="stHeader"] {{
-    # background: rgba(0,0,0,0);
-    # }}
-
-    # [data-testid="stToolbar"] {{
-    # right: 2rem;
-    # }}
-    # </style>
-    # """
-
-    # st.markdown(page_bg_img, unsafe_allow_html=True)
-
-    person1 = {
-        "name": "Batata 1",
-        "photo": Image.open("images/potato.jpg"),
-        "linkedin": "https://www.linkedin.com/in/Nuno/"
-    }
-
-    person2 = {
-        "name": "Batata 2",
-        "photo": Image.open("images/potato.jpg"),
-        "linkedin": "https://www.linkedin.com/in/Vick/"
-    }
-
-    person3 = {
-        "name": "Batata 3",
-        "photo": Image.open("images/potato.jpg"),
-        "linkedin": "https://www.linkedin.com/in/Rafa/"
-    }
-
-    st.title("Manda um Alô!")
-
-    contact_info = st.container()
-
-    col1, col2, col3 = contact_info.columns(3)
-
-    with col1:
-        st.image(person1["photo"], use_column_width=True)
-        st.write(f"**{person1['name']}**")
-        st.write(f"[LinkedIn]({person1['linkedin']})")
-
-    with col2:
-        st.image(person2["photo"], use_column_width=True)
-        st.write(f"**{person2['name']}**")
-        st.write(f"[LinkedIn]({person2['linkedin']})")
-
-    with col3:
-        st.image(person3["photo"], use_column_width=True)
-        st.write(f"**{person3['name']}**")
-        st.write(f"[LinkedIn]({person3['linkedin']})")
-
-    contact_info.header("Manda um Alô!")
-    contact_info.markdown("### Nosso Time")
-    contact_info.markdown("#### Agradecemos o seu interesse e aceitamos feedbacks.")
-
-    st.write('---')
-
-    st.write('Copyright 2024. Todos os direitos reservados.')
+    
+    st.subheader('GERADOR')
+    st.subheader('aqui conta conto')
+    
+    def button_gerador():
+        st.session_state["escolha"] = 'pergunta'
+    
+    confirm_button = st.button('Reconfigurar personagem', on_click=button_gerador)
