9e509f57c2128324442af161e81d65442d52b05e
Nuno Fernandes <nmfernandes@hotmail.com>
2024-06-17 10:03:47 -0300

    copy paste pasta rafa _01

--- project/streamlit_app/contato_nuno.py
+++ project/streamlit_app/contato_nuno.py
@@ -10,11 +10,46 @@ def contato() -> None:
     # st.subheader('GERADOR')
     # st.subheader('aqui conta conto')
 
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def button_pergunta():
+    st.session_state["escolha"] = 'gerador'
+
+def home():
+    # st.subheader('HOME')
+    # st.subheader('aqui faz perguntas')
+
+    img = get_img_as_base64("project/streamlit_app/images/clouds.png")
+
+    page_bg_img = f"""
+    <style>
+    [data-testid="stAppViewContainer"] > .main {{
+    background-image: url("data:image/png;base64,{img}");
+    background-size: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: local;
+    }}
+
+    [data-testid="stHeader"] {{
+    background: rgba(0,0,0,0);
+    }}
+
+    [data-testid="stToolbar"] {{
+    right: 2rem;
+    }}
+    </style>
+    """
+
+    # st.markdown(page_bg_img, unsafe_allow_html=True)
+
     st.markdown(
     f"""
-    <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
-    >
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap">
     """,
     unsafe_allow_html=True,
     )
@@ -31,74 +66,92 @@ def contato() -> None:
         unsafe_allow_html=True,
     )
 
-    st.title("Gerador de Contos")
+    if "nome" not in st.session_state:
+        st.session_state["nome"] = ""
 
-    nome = st.session_state["nome"]
-    idade = st.session_state["idade"]
-    tema = st.session_state["tema"]
-    corpo = st.session_state["corpo"]
+    st.subheader("Nome do Personagem")
+    nome = st.text_input(f"Nome do Personagem: ", value=st.session_state["nome"])
 
-    if tema == "Tema 1":
-        texto_tema = "estava procurando um dragão em forma de batata frita"
+    # nome_button = st.button("Confirmar Nome")
 
-    if tema == "Tema 2":
-        texto_tema = "estava com medo de unicórnios voadores"
-
-    if tema == "Tema 3":
-        texto_tema = "procurava uma vaga para estacionar o seu cavalo"
-
-    if tema == "Tema 4":
-        texto_tema = "andava pelo jardim de sorvetes florais"
-
-    if tema == "Tema 5":
-        texto_tema = "passeava com seu cachorro Fluffy"
-
-    if corpo in ["Garoto", "Jovem", "Senhor"]:
-        artigo = "um"
-        sufixo = "o"
-    else:
-        artigo = "uma"
-        sufixo = "a"
-
-    if tema:
-        contexto = f"Há muitos anos, em uma galáxia medieval distante, {artigo} {corpo.lower()} de {idade} anos, chamad{sufixo} {nome}, {texto_tema}..."
-
-        formatted_md = f"#### **{contexto}**"
-
-        st.subheader("Ideia inicial: ")
-        st.markdown(formatted_md, unsafe_allow_html=True)
-
-        # contexto entra no modelo de tradução
-        # contexto traduzido vai para o modelo gerador de texto
-        # output do modelo gerador de texto é resumido
-        # resumo e output inteiros são traduzidos
-        # resumo é mostrado em 3 opções
-        # usuário escolhe uma das opções
-        # output é mostrado em uma caixa de texto
-
-        # opcoes de escolha
-    #     selected_option = st.selectbox("Escolha uma opção", options)
-
-    #     # Update the text box with the new value
-    #     if selected_option == options[0]:
-    #         text_box = completo_1
-    #     elif selected_option == options[1]:
-    #         text_box = completo_2
-    #     elif selected_option == options[2]:
-    #         text_box = completo_3
+    # if nome_button:
+    #     st.session_state["nome"] = nome
+    #     # st.write("Nome escolhido: ", nome)
+    #     st.success(f"Nome escolhido: {nome}")
 
 
-    #     caixa de texto st.session_state[caixa de texto]
+    if "corpo" not in st.session_state:
+        st.session_state["corpo"] = ""
 
-    #     select box -> alterar caixa de texto
+    st.subheader("Tipo de Personagem")
 
-    #     texto_caixa de texto -> alteracao no session state do input modelo
+    lista_tipo = [" ",
+                   "Humano",
+                   "Elfo",
+                   "Unicórnio",
+                   "Ogro",
+                   "Fada",
+                   "Anão"]
+    corpo = st.selectbox("Tipo de personagem", lista_tipo)
 
-    #     input - > executar o request
+    # if corpo == " ":
+    #     st.error("Escolha o tipo")
 
-    #     st.session
+    # corpo_button = st.button("Confirmar Corpo")
 
-    # confirm_button = st.button('Reconfigurar personagem', on_click=button_gerador)
+    # if corpo_button:
+    #     st.session_state["corpo"] = corpo
+    #     # st.write("Tipo de corpo escolhido: ", corpo)
+    #     st.success(f"Tipo de corpo escolhido: {corpo}")
 
+    if "idade" not in st.session_state:
+        st.session_state["idade"] = 0
 
-    # confirm_button = st.button('Reconfigurar personagem', on_click=button_gerador)
+    st.subheader("Idade do Personagem")
+    idade = st.slider("Idade do Personagem: ",
+                      min_value=10,
+                      max_value=80,
+                      step=1,
+                      value=st.session_state["idade"],
+                    #   width=400,
+                    #   height=200
+                      )
+
+    # idade_button = st.button("Confirmar Idade")
+
+    # if idade_button:
+    #     st.session_state["idade"] = int(idade)
+    #     # st.write("Idade escolhida: ", idade, "anos")
+    #     st.success(f"Idade escolhida: {idade} anos")
+
+    if "tema" not in st.session_state:
+        st.session_state["tema"] = []
+
+    st.subheader("Tema do Conto")
+
+    lista_tema = ["Tema do Conto", "Tema 1", "Tema 2", "Tema 3", "Tema 4", "Tema 5"]
+    tema = st.selectbox("Tema do Conto", lista_tema)
+
+    # tema_button = st.button("Confirmar Tema")
+
+    # if tema_button:
+    #     st.session_state["tema"] = tema
+    #     # st.write("Tema escolhido: ", st.session_state["tema"])
+    #     st.success(f"Tema escolhido: {tema}")
+
+    # if selected_page == "Confirmar":
+    if nome == " ":
+        nome = "Introduza o nome"
+    elif nome != " ":
+        st.session_state["nome"] = nome
+    st.session_state["idade"] = int(idade)
+    st.session_state["corpo"] = corpo
+    st.session_state["tema"] = tema
+
+    # if qualquer nao existe:
+    #     button
+    #     st.error(DEFINA A MERDA DO NOME)
+
+    # else:
+
+    confirm_button = st.button('Confirmar escolhas', on_click=button_pergunta)
