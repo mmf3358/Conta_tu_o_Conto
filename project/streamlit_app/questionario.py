819b66dc3622a63058b20131d4e673533f9d9025
Nuno Fernandes <nmfernandes@hotmail.com>
2024-06-21 21:07:54 -0300

    final

--- project/streamlit_app/questionario.py
+++ project/streamlit_app/questionario.py
@@ -1,57 +1,179 @@
 import streamlit as st
 import os
-import base64
-
-from project.streamlit_app.streamlit_utils import *
-
+import random
+from streamlit_utils import *
 
 
-@st.cache_data
-def get_base64_of_bin_file(bin_file):
-    ###  Codificador da imagem
+
+def Titulo_do_conto():
+    ### Definir titulo
+    subtitulo = "Titulo do Conto"
+    st.markdown(f'<p class="subtitulo_01">{subtitulo}</p>', unsafe_allow_html=True)
     
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
+    st.session_state["titulo"] = st.text_input("Titulo", value=None)
 
 
 
-def Define_imagem_de_fundo(png_file):
-    ###  Define a imagem de fundo
-    
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-        <style>
-        .stApp {
-        background-image: url("data:image/png;base64,%s");
-        background-size: cover;
-        }
-        </style>
-        ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
+def Dados_personagem():
+    ### Definir os dados do personagem
+    subtitulo = "Dados do Personagem"
+    st.markdown(f'<p class="subtitulo_01">{subtitulo}</p>', unsafe_allow_html=True)
+
+    nome = st.text_input("Nome", value=None)
+    idade = st.text_input("Idade", value=None)
+    ocupacao = st.text_input("Ocupação", value=None)
+
+    st.session_state["dados"] = [nome, idade, ocupacao]
 
 
-def questionario():
-    
-    st.header('la la la')
-    
-    
-    cwd = os.getcwd()
-    bg_image_file = f"{cwd}/project/streamlit_app/images/Fundo_01 copy.png"
-    style_css_file = f"{cwd}/project/streamlit_app/style.css"
 
-    with open( style_css_file ) as css:
-        st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+def Selecionar_tema():
+    ### Definir o tema do contexto
+    col1, col2 = st.columns([6, 8])
 
-    
-    Define_imagem_de_fundo(bg_image_file)
-    
+    subtitulo = "Tema do Conto"
+    st.markdown(f'<p class="subtitulo_01">{subtitulo}</p>', unsafe_allow_html=True)
+
+    button_escolha_01 = st.session_state["tema_botao"][0]
+    button_escolha_02 = st.session_state["tema_botao"][1]
+    button_escolha_03 = st.session_state["tema_botao"][2]
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.markdown(f'<span id={button_escolha_01}></span>', unsafe_allow_html=True)
+        st.button(
+            "Tema Castelo",
+            key = random.randint(0,42424242),
+            on_click=Definir_tema,
+            args=(0,),
+            use_container_width=True)
+    with col2:
+        st.markdown(f'<span id={button_escolha_02}></span>', unsafe_allow_html=True)
+        st.button(
+            "Tema Floresta",
+            key = random.randint(0,42424242),
+            on_click=Definir_tema,
+            args=(1,),
+            use_container_width=True)
+    with col3:
+        st.markdown(f'<span id={button_escolha_03}></span>', unsafe_allow_html=True)
+        st.button(
+            "Tema Fadas",
+            key = random.randint(0,42424242),
+            on_click=Definir_tema,
+            args=(2,),
+            use_container_width=True
+            )
+
+
+
+def Definir_tema(opcao):
+    ### Definir estilo do botao do tema
+    st.session_state["tema_botao"] = ['"button-escolha"','"button-escolha"','"button-escolha"']
+    st.session_state["tema_botao"][opcao] = '"button-escolhido"'
+    st.session_state["tema"] = opcao
+
+
 
 def alterar_estado():
+    ### Mudar estado da pagina
+    st.session_state["tentativa_alt_estado"] = True
+    
+    if st.session_state["titulo"] == None:
+        st.session_state["erro"] = 3
+        return 0
+    if Verifica_dados() == True:
+        st.session_state["erro"] = 2
+        return 0
+    if st.session_state["tema"] == None:
+        st.session_state["erro"] = 1
+        return 0
+            
+    st.session_state["contexto"] = Gerar_Contexto()
     st.session_state["estado_inicial"] = 'gerador'
 
+
+
+def Gerar_Contexto():
+    ### Gerar contexto a partir dos dados
+    nome = st.session_state["dados"][0]
+    idade = st.session_state["dados"][1]
+    ocupacao = st.session_state["dados"][2]
+    tema = st.session_state["tema"]
+
+    temas = [f"Era uma vez, um {ocupacao} chamado {nome}. Tinha {idade} anos e gostava muito de aventuras em lugares misteriosos. Um dia encontrou um castelo que nunca havia visto na vida.",
+             f"Numa floresta mágica, um elfo {ocupacao} chamado {nome}, de idade {idade} anos, descobriu um portal secreto que a levaria a um mundo feito de doces, onde os animais falavam a língua dos elfos.",
+             f"Nos campos do Reino de Chocolate, a pequena fada {nome} era perseguida pela Rainha Malévola, que a ameaçava de aprisioná-la em um sono eterno se não revelasse os segredos da floresta mágica."]
+
+    return temas[tema]
+
+
+
+def Verifica_dados():
+    ### Verifica a introdução dos dados
+    for i in range(3):
+        if st.session_state["dados"][i] == None or st.session_state["tema"] == "":
+            return True
+        else:
+            return False
+
+
+
 def questionario():
-    st.subheader("questionario")
-    confirm_button = st.button('gerar conto', on_click=alterar_estado)
+    ### Cria variavel de corrente
+    cwd = os.getcwd()
+    
+    ### Importa estilos CSS
+    style_css_file = f"{cwd}/project/streamlit_app/style.css"
+    
+    with open(style_css_file) as css:
+        st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+
+    ### Define Estados iniciais
+    Estados_iniciais()
+
+    ### Imprime imagem de fundo e titulo
+    bg_image_file = f"{cwd}/project/streamlit_app/images//fundo_questionario.png"
+    Define_imagem_de_fundo(bg_image_file)
+    titulo = "Conta Tu o Conto"
+    st.markdown(f'<p class="titulo">{titulo}</p>', unsafe_allow_html=True)
+    
+    ### Define o titulo do conto
+    Titulo_do_conto()
+    
+    st.subheader("")
+    st.subheader("")
+    
+    ### Pega dados do personagem
+    Dados_personagem()
+
+    st.subheader("")
+
+    ### Define o tema do contexto
+    Selecionar_tema()
+
+    st.subheader("")
+    st.subheader("")
+
+    ### Verifica o preenchimento
+    if st.session_state["erro"] == 0:
+        texto = "      "
+        st.markdown(f'<p class="texto_02">{texto}</p>', unsafe_allow_html=True)
+    elif st.session_state["erro"] == 1:
+        texto = "Por favor, defina um Título."
+        st.markdown(f'<p class="texto_02>{texto}</p>', unsafe_allow_html=True)
+    elif st.session_state["erro"] == 2:
+        texto = "Por favor, introduza os dados."
+        st.markdown(f'<p class="texto_02">{texto}</p>', unsafe_allow_html=True)
+    elif st.session_state["erro"] == 3:
+        texto = "Por favor, defina um Tema."
+        st.markdown(f'<p class="texto_02">{texto}</p>', unsafe_allow_html=True)
+        
+    ### Botao de geração do conto
+    col1, col2, col3 = st.columns([4,8,4])
+    with col2:
+        st.markdown('<span id="button-after"></span>', unsafe_allow_html=True)
+        st.button(
+            'Gerar Conto',
+            on_click=alterar_estado,
+            use_container_width=True)
--- project/streamlit_app/streamlit_utils.py
+++ project/streamlit_app/streamlit_utils.py
@@ -1,11 +1,10 @@
 import streamlit as st
-import os
 import base64
 
-@st.cache_data
+
 def get_base64_of_bin_file(bin_file):
     ###  Codificador da imagem
-    
+
     with open(bin_file, 'rb') as f:
         data = f.read()
     return base64.b64encode(data).decode()
@@ -14,7 +13,7 @@ def get_base64_of_bin_file(bin_file):
 
 def Define_imagem_de_fundo(png_file):
     ###  Define a imagem de fundo
-    
+
     bin_str = get_base64_of_bin_file(png_file)
     page_bg_img = '''
         <style>
@@ -24,7 +23,39 @@ def Define_imagem_de_fundo(png_file):
         }
         </style>
         ''' % bin_str
-    
+
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
+
+
+
+def Estados_iniciais():
+    ### Inicializa os estados inicias das variaveis persistentes
+    if "tema_botao" not in st.session_state:
+        st.session_state["tema_botao"] = ['"button-escolha"','"button-escolha"','"button-escolha"']
+
+    if "tema" not in st.session_state:
+        st.session_state["tema"] = None
+
+    if "dados" not in st.session_state:
+        st.session_state["dados"] = [None, None, None]
+
+    if "opcao" not in st.session_state:
+        st.session_state["opcao"] = ""
+
+    if "contexto" not in st.session_state:
+        st.session_state["contexto"] = ""
+
+    if "tentativa_alt_estado" not in st.session_state:
+        st.session_state["tentativa_alt_estado"] = False
+        
+    if "opcao" not in st.session_state:
+        st.session_state["opcao"] = ""
+        
+    if "titulo" not in st.session_state:
+        st.session_state["titulo"] = None
+        
+    if "erro" not in st.session_state:
+        st.session_state["erro"] = 0
+
 
